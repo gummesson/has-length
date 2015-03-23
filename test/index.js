09caf51ab441e7e58195640cf10d5db744af3860
Ellen Gummesson <ellengummesson@gmail.com>
2015-03-23 14:27:34 +0100

    Simplify tests.

--- test/index.js
+++ test/index.js
@@ -10,11 +10,11 @@ var hasLength = require('../')
  */
 
 test('hasLength(obj)', function(assert) {
-  assert.equal(hasLength([1]), true, 'returns true for arrays')
-  assert.equal(hasLength({ one: 1 }), true, 'returns true for objects')
-  assert.equal(hasLength('string'), true, 'returns true for strings')
-  assert.equal(hasLength([]), false, 'returns false for empty arrays')
-  assert.equal(hasLength({}), false, 'returns false for empty objects')
-  assert.equal(hasLength(''), false, 'returns false for empty strings')
+  assert.equal(hasLength([1]), true)
+  assert.equal(hasLength({ one: 1 }), true)
+  assert.equal(hasLength('string'), true)
+  assert.equal(hasLength([]), false)
+  assert.equal(hasLength({}), false)
+  assert.equal(hasLength(''), false)
   assert.end()
 })
